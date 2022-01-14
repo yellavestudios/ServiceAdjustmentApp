eaf32a78179ed0c59e8782fecbfc1da6c3bba224
Christopher Pearl <53843747+yellavestudios@users.noreply.github.com>
2022-01-13 20:23:13 -0800

    Add files via upload

--- AWSManager.cs
+++ AWSManager.cs
@@ -14,22 +14,50 @@ using Amazon;
 public class AWSManager : MonoBehaviour
 {
 
+    public string S3Region = RegionEndpoint.USEast1.SystemName;
+    //property from AWS
+    private RegionEndpoint _S3Region;
+    {
+        get { return RegionEndpoint.GetBySystemName(S3Region); 
+    }
 
+    
 
-
-  private void Awake()
+    private void Awake()
     {
 
         UnityInitializer.AttachToGameObject(this.gameObject);
+        //Use Unity Web Request
+        AWSConfigs.HttpClient = AWSConfigs.HttpClientOption.UnityWebRequest;
 
-
-        CognitoAWSCredentials credentials = new CognitoAWSCredentials(
+    //initialize credentials with AWS
+    CognitoAWSCredentials credentials = new CognitoAWSCredentials(
         "us-east-1:6afff219-0978-43dc-9314-70dd4f14292f", // Identity Pool ID
         RegionEndpoint.USEast1 // Region
         );
 
+        //S3 client
+        IAmazonS3 s3Client = new AmazonS3Client(<credentials>, _S3Region);
 
-        IAmazonS3 s3Client = new AmazonS3Client(< credentials >, RegionEndpoint.USEast1);
 
+
+        // ResultText is a label used for displaying status information
+        
+        S3Client.ListBucketsAsync(new ListBucketsRequest(), (responseObject) =>
+        {
+            
+            if (responseObject.Exception == null)
+            {
+                
+                responseObject.Response.Buckets.ForEach((s3b) =>
+                {
+                    Debug.Log("Bucket Name: " + s3b.BucketName);
+                };
+            }
+            else
+            {
+                Debug.Log("AWS Error");
+            }
+        });
     }
 }
